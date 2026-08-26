75db25f7f039c13d99e96c86b7319e88c0aa2b5a
Dhruv Singhania <dhruv.s0611@gmail.com>
2026-08-26 17:51:56 +0530

    created the email service

---
+++ backend/src/services/email/email.service.ts
@@ -0,0 +1,29 @@
+import nodemailer from "nodemailer";
+import "dotenv/config";
+
+const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: Number(process.env.SMTP_PORT),
+    secure: process.env.SMTP_SECURE === "true",
+    auth: {
+        user: process.env.SMTP_USER,
+        pass: process.env.SMTP_PASSWORD
+    },
+});
+
+export async function sendEmail(to: string, subject: string, text: string) {
+    await transporter.sendMail({
+        from: process.env.SMTP_FROM,
+        to,
+        subject,
+        text,
+    });
+}
+
+export async function sendRegistrationOtpEmail(email: string, otp: string) {
+    await sendEmail(
+        email,
+        "SellOnCampus Email Verification",
+        `Your SellOnCampus verification code is ${otp}. This code expires in 2 minutes`
+    );
+}
